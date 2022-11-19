9ae295c36729a0ef11b94c47b61ca0b03f18a5dd
IvanBSupc <ivan.bermudez@estudiantat.upc.edu>
2022-11-19 21:39:45 +0100

    Velocidad palas

--- ModulePlayer.cpp
+++ ModulePlayer.cpp
@@ -35,12 +35,12 @@ update_status ModulePlayer::Update()
 	if (App->input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT)
 		App->scene_intro->palancaleft->body->ApplyTorque(-60.0f, true);
 	else
-		App->scene_intro->palancaleft->body->ApplyTorque(15.0f, true);
+		App->scene_intro->palancaleft->body->ApplyTorque(5.0f, true);
 
 	if (App->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
 		App->scene_intro->palancaright->body->ApplyTorque(60.0f, true);
 	else
-		App->scene_intro->palancaright->body->ApplyTorque(-15.0f, true);
+		App->scene_intro->palancaright->body->ApplyTorque(-5.0f, true);
 	
 	return UPDATE_CONTINUE;
 }
